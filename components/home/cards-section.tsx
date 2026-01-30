8f2e6145f06f5d4202c656a82cb4455890f4882b
Shafly Hamzah <shafly2000@gmail.com>
2026-01-30 10:50:39 +0700

    feat: update CardsSection layout and content; add new card images

--- components/home/cards-section.tsx
+++ components/home/cards-section.tsx
@@ -3,38 +3,37 @@ import Image from "next/image";
 import { Container } from "@/components/common/container";
 import { Card } from "@/components/ui/card";
 
-const phoneSrc = "/images/hero-phone.avif";
+const cardSrc = "/images/visa-card.avif";
+const phoneCardSrc = "/images/phone-visa-card.avif";
 
 export function CardsSection() {
   return (
     <section>
       <Container>
         <div className="grid gap-6 md:grid-cols-2">
-          <Card className="rounded-3xl border-none bg-slate-50 p-6">
+          <Card className="rounded-3xl border-none bg-slate-100 p-8 text-black h-128 overflow-hidden">
             <div className="space-y-4">
-              <h3 className="text-lg font-semibold text-slate-900">
-                Physical Card
-              </h3>
-              <p className="text-sm text-slate-500">
-                Tap anywhere Visa is accepted with a premium physical card.
+              <h3 className="text-4xl font-semibold"> Physical Card</h3>
+              <p className="text-xl font-medium text-slate-500">
+                Get an Avici Physical Card delivered to your doorstep.
               </p>
               <Image
-                src={phoneSrc}
-                alt="Physical card preview"
+                src={cardSrc}
+                alt="Digital wallet preview"
                 width={520}
                 height={520}
                 className="h-auto w-full"
               />
             </div>
           </Card>
-          <Card className="rounded-3xl border-none bg-black p-6 text-white">
+          <Card className="rounded-3xl border-none bg-slate-100 p-8 text-black h-128 overflow-hidden">
             <div className="space-y-4">
-              <h3 className="text-lg font-semibold">Tap to Pay</h3>
-              <p className="text-sm text-white/70">
-                Pay with Apple Pay or Google Pay in seconds.
+              <h3 className="text-4xl font-semibold">Tap to Pay</h3>
+              <p className="text-xl font-medium text-slate-500">
+                Shop offline like a pro by adding to Apple Pay or Google pay
               </p>
               <Image
-                src={phoneSrc}
+                src={phoneCardSrc}
                 alt="Digital wallet preview"
                 width={520}
                 height={520}
